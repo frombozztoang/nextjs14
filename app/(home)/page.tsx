e3597878f1969b0b5006d0f27767d4e09bc9d8a8
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-03-09 15:12:04 +0900

    0309: fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,16 @@
-import Navigation from "../../components/navigation";
-
 export const metadata = {
-  title: "Home",
+  title: "home",
 };
 
-export default function Tomato() {
-  return <div>hello </div>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+const getMovies = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+};
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)} </div>;
 }
