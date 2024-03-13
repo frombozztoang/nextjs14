397e30bcbadc5c1c33ffe1bd7f1e05d166f60509
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-03-13 21:10:04 +0900

    0313: parallel requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,31 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
+// fetch 함수, server component
 const getMovies = async () => {
+  // 일부러 늦추는 함수
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(URL);
   const json = await response.json();
   return json;
 };
 
+// async를 사용하는 이유는 await을 사용하기 때문
+// await을 쓰려면 부모 함수가 async여야함
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)} </div>;
+  return (
+    <div>
+      {movies.map((m) => (
+        <li key={m.id}>
+          <Link href={`/movies/${m.id}`}>{m.title}</Link>
+        </li>
+      ))}{" "}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,24 @@
-const page = ({ params: { id } }: { params: { id: string } }) => {
-  console.log(id);
-  return <div>Movie {id}</div>;
+import { URL } from "../../../(home)/page";
+
+const getMovie = async (id: string) => {
+  console.log(`fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${URL}/${id}`);
+  return response.json();
+};
+
+const getVideos = async (id: string) => {
+  console.log(`fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${URL}/${id}/videos`);
+  return response.json();
+};
+
+const page = async ({ params: { id } }: { params: { id: string } }) => {
+  console.log("=====start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("======end fetching");
+  return <h1>{movie.title}</h1>;
 };
 
 export default page;
